2454c6ad9818077c1fd2492c2b8234ad46da506d
Franklin Ho <franklineho@gmail.com>
2015-01-22 15:07:57 -0800

    added conversion endpoint

--- adserver/urls.py
+++ adserver/urls.py
@@ -4,4 +4,5 @@ from adserver import views
 
 urlpatterns = patterns('',
                        url(r'^$', views.index, name = 'index'),
-                       url(r'^int$', views.detail, name = 'detail'),)
+                       url(r'^int$', views.intent, name = 'intent'),
+                       url(r'^conv', views.conversion, name = 'conversion'),)
--- adserver/views.py
+++ adserver/views.py
@@ -9,7 +9,7 @@ from adserver.models import Intent
 def index(request):
     return HttpResponse("Hello, world. You're at the adserver index.")
 
-def detail(request):
+def intent(request):
     idfa = request.GET.get('idfa')
     product_id = request.GET.get('product_id')
     advertiser_id = request.GET.get('advertiser_id')
@@ -20,11 +20,34 @@ def detail(request):
     #     'advertiser_id', advertiser_id
     # }
 
-    intents = Intent.objects.filter(idfa=idfa,product_id=product_id,advertiser_id=advertiser_id)
+    if idfa is not None and product_id is not None and advertiser_id is not None:
+        intents = Intent.objects.filter(idfa=idfa,product_id=product_id,advertiser_id=advertiser_id)
 
-    if intents.count() == 0:
-        i = Intent(idfa=idfa, product_id=product_id, advertiser_id = advertiser_id, converted=False)
-        i.save()
-        return HttpResponse("Tracking Intent. IDFA is %s. Product ID is %s. Advertiser ID is %s" % (idfa,product_id,advertiser_id))
+        if intents.count() == 0:
+            i = Intent(idfa=idfa, product_id=product_id, advertiser_id = advertiser_id, converted=False)
+            i.save()
+            return HttpResponse("Tracking Intent. IDFA is %s. Product ID is %s. Advertiser ID is %s" % (idfa,product_id,advertiser_id))
+        else:
+            return HttpResponse("Intent for this combination already exists. IDFA is %s. Product ID is %s. Advertiser ID is %s" % (idfa,product_id,advertiser_id))
     else:
-        return HttpResponse("Intent for this combination already exists. IDFA is %s. Product ID is %s. Advertiser ID is %s" % (idfa,product_id,advertiser_id))
+        return HttpResponse("Missing parameters. Are you sure you have the idfa, the product_id and the advertiser_id?")
+
+def conversion(request):
+    idfa = request.GET.get('idfa')
+    product_id = request.GET.get('product_id')
+    advertiser_id = request.GET.get('advertiser_id')
+
+    if idfa is not None and product_id is not None and advertiser_id is not None:
+        intents = Intent.objects.filter(idfa=idfa,product_id=product_id,advertiser_id=advertiser_id)
+        print(intents)
+        if intents.count() == 0:
+            return HttpResponse("No associated retargeting intent. IDFA is %s. Product ID is %s. Advertiser ID is %s" % (idfa,product_id,advertiser_id))
+        elif intents[0].converted == False:
+            i = intents[0]
+            i.converted = True
+            i.save()
+            return HttpResponse("Confirming conversion for this intent. IDFA is %s. Product ID is %s. Advertiser ID is %s" % (idfa,product_id,advertiser_id))
+        else:
+            return HttpResponse("This intent has already been confirmed. IDFA is %s. Product ID is %s. Advertiser ID is %s" % (idfa,product_id,advertiser_id))
+    else:
+        return HttpResponse("Missing parameters. Are you sure you have the idfa, the product_id and the advertiser_id?")
